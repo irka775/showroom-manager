2414e67e86e7f50e7b41161ff57bed2a05000d53
irka775 <barbascumpa54@gmail.com>
2024-03-02 22:11:08 +0000

    Add library in requirements and call main fun

--- run.py
+++ run.py
@@ -1,2 +1,13 @@
+import os
+from colorama import Fore, Back, Style
+from time import sleep
+from num2words import num2words
+from pathlib import Path
+
+
 def main():
     pass
+
+
+if __name__ == "__main__":
+    main()
