496d74d5bf192916991526bfbc8bd77aff62aee3
irka775 <barbascumpa54@gmail.com>
2024-03-04 16:12:24 +0000

    Bug fixing,number of data in show data function

---
+++ README.md
@@ -0,0 +1,144 @@
+# Showroom Manager
+
+Sowroom Manager represents a robust console-based application designed to manage the inventory
+of a car showroom efficiently. This project aims to streamline the process of adding, modifying, deleting,
+and viewing car details, offering a user-friendly interface for showroom management tasks. The application
+leverages various functionalities such as input validation,
+file handling, and data manipulation to ensure accurate and seamless management of car inventory.
+This Python console offers a comprehensive solution for organizing and maintaining a car showroom's inventory,
+catering to the needs of showroom managers and facilitating efficient management of car listings through a simple
+yet powerful console interface. This helps new users get acquainted with the application quickly.
+
+## Features
+
+- **Welcome Message and Instructions**:
+  Display a welcome message and instructions when the application starts
+  to guide users on how to use the different features of the show from the showroom inventory. Users should be able
+  to specify the ID of the car they want to delete, and the corresponding car entry should be removed from the inventory.
+
+![Welcome msg](assets/screenshots/welcome_screenshot.JPG "Welcome message")
+
+- **User-Friendly Interface**:
+  Design a user-friendly interface using color-coding and clear prompts to guide users
+  through different actions such as adding, modifying, deleting, or viewing cars in the showroom.
+
+![Menu](assets/screenshots/app_screenshot.JPG "Menu screenshot")
+
+- **Add Car Functionality**:
+  Allow users to add new cars to the showroom inventory by providing details such as brand, model,
+  fuel type, engine size, transmission, year, mileage, color, and price.
+
+![Add car](assets/screenshots/addCar_screenshot.JPG "Add car screenshot")
+
+- **Modify Car Functionality**:
+  Enable users to modify the details of existing cars in the showroom inventory.
+  This feature should allow users to select a car by its ID and update its attributes.
+- **Delete Car Functionality**:
+  Provide the capability to remove cars from the showroom inventory. Users should be able to specify
+  the ID of the car they want to delete, and the corresponding car entry should be removed from the inventory.
+- **Show Cars Functionality**:
+  Implement a feature to display all cars currently present in the showroom inventory.
+  This functionality should show detailed information about each car, including its ID, brand, model, fuel type, etc.
+
+![Show cars](assets/screenshots/addCar_screenshot.JPG "Show cars screenshot")
+
+- **Total Cars Functionality**:
+  Show the total number of cars present in the showroom inventory. This feature provides
+  users with a quick overview of the current size of the inventory.
+
+![Total cars](assets/screenshots/totalCars_screenshot.JPG "Total cars screenshot")
+
+- **Exit**:
+  Exit the application.
+- **Data Persistence**:
+  Implement data persistence mechanisms to ensure that the showroom inventory data is saved between
+  sessions. This ensures that the data is not lost when the application is closed or restarted.
+- **Input Validation**:
+  Validate user input to ensure that only valid data is accepted. Implement checks to verify that
+  input values adhere to specified formats and ranges (e.g., numeric fields should only accept numbers).
+- **Error Handling**:
+  Handle errors gracefully by providing informative error messages to users when unexpected issues occur.
+  This helps users understand the nature of the problem and how to address it.
+
+## Future Features
+
+- **Search and Filter Functionality** :  
+  Allow users to search for specific cars or filter cars based on criteria such as brand,
+  model, price range, or year.
+- **Image Upload and Display**:  
+  Enable users to upload images of cars to accompany their details in the inventory.
+- **Sorting Options**:  
+  Provide options to sort the showroom inventory based on various attributes such as price, year, mileage, or brand.
+- **User Authentication and Authorizations**:  
+  Implement user authentication and authorization mechanisms to restrict access to certain features or data
+  based on user roles.
+- **Integration with External Systems**:  
+  Integrate the showroom management system with other systems or platforms, such as accounting software, customer
+  relationship management (CRM) systems, or online marketplaces.
+
+## Testing
+
+I have manually tested this code by doing the following:
+
+- Passed the code through a PEP8 linter and confirmed there are no problems
+- Test each menu option (adding a car, modifying a car, deleting a car, showing cars,
+  displaying the total number of cars, and exiting) to ensure they perform the intended actions.
+- Enter different types of input (valid, invalid) when prompted and verify that the
+  program handles them correctly.
+- Test each data type validation (alphanumeric, numeric, float) with valid and invalid inputs.
+- Check for any error messages or unexpected behavior during execution.
+- Verify that the program provides appropriate error messages and prompts
+  the user to re-enter input when validation fails.
+- Verify that the file is created if it doesn't exist and that existing data is
+  overwritten or appended as expected.
+- Test each function in the code to ensure it performs its intended functionality accurately.
+  For example, when adding a car, verify that the car details are correctly added to the showroom
+  data and written to the file or similarly, when modifying or deleting a car, verify that
+  the specified car is updated or removed from the showroom data.
+- Tested in my local terminal and the Code Institute Heroku terminal.
+
+## Validator Testing
+
+- PEP8:  
+  No errors were returned from PEP8 online.com
+
+![Python Validation](assets/screenshots/python-linter.JPG "Screenshot of Python validation")
+
+## Deployment
+
+- Steps for deployment:  
+  Fork or clone this repository  
+  Create a new Heroku app  
+  Set the buildbacks to Python and NodeJS in that order  
+  Link the Heroku app to the repository  
+  Click on Deploy
+
+## Installation
+
+1. Clone or download the repository.
+2. Ensure you have Python installed on your system (Python 3.6 or later).
+3. Install the required dependencies using pip:
+
+   ```bash
+   pip install colorama num2words
+   ```
+
+## Usage
+
+Run the main Python script to start the Showroom Manager:
+
+    python showroom_manager.py
+
+## License
+
+Copyright (c) 2024
+
+Permission is hereby granted, free of charge,
+
+THE SOFTWARE IS PROVIDED "AS IS",WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ run.py
@@ -0,0 +1,420 @@
+import os
+import threading
+import time
+from colorama import Fore, Back, Style
+from time import sleep
+from num2words import num2words
+from pathlib import Path
+
+file_name = "ireland_bray_showroom_stock.txt"
+LOG_FILE_NAME = os.getenv("TASKS_LOG_PATH", file_name)
+
+if os.path.isabs(LOG_FILE_NAME):
+    full_path = LOG_FILE_NAME
+else:
+    desktop_path = Path.home() / "Desktop"
+    full_path = desktop_path / LOG_FILE_NAME
+
+
+# ============================================================================
+space_menu_count = 2
+menu_str = (
+    f"{Fore.CYAN}1:{' ' * space_menu_count}Add_car\n"
+    f"{Fore.CYAN}2:{' ' * space_menu_count}Modify_car\n"
+    f"{Fore.YELLOW}3:{' ' * space_menu_count}Delete_car\n"
+    f"{Fore.YELLOW}4:{' ' * space_menu_count}Show_Cars\n"
+    f"{Fore.RED}5:{' ' * space_menu_count}Total_cars\n"
+    f"{Fore.RED}6:{' ' * space_menu_count}Exit\n"
+)
+
+showroom_data = []
+header_line = f"{'='*23}  Showroom data  {'='*23}"
+separator_line = "=" * 63
+car_count = str(len(showroom_data))
+
+
+header = {
+    "ID": "",
+    "Brand": "alfabetic",
+    "Model": "alfanumeric",
+    "Fuel_Type": "alfabetic",
+    "Engine_Size": "float",
+    "Transmission": "alfabetic",
+    "Year": "numeric",
+    "Mileage": "numeric",
+    "Color": "alfabetic",
+    "Price": "numeric",
+}
+
+
+# ============================================================================
+
+
+def msg_count_car():
+    """
+    Prints a message indicating the total number of cars in the showroom.
+    This total is converted to words for a friendly user interface.
+    """
+
+    message = (
+        f"Hey there! We've got {num2words(car_count).upper()} "
+        "awesome cars waiting for you in the showroom!\n"
+    )
+    print(Fore.RED + message)
+    input(Fore.RED + "Press Enter to continue...")
+
+
+# ============================================================================
+
+
+def clear_screen():
+    """
+    Clears the console screen based on the operating system.
+    """
+    # Check if the operating system is Windows
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        # Assume the operating system is Unix/Linux/Mac
+        os.system("clear")
+
+
+# ============================================================================
+
+
+def exist(path):
+    """
+    Checks if a given path exists in the file system.
+
+    Returns:
+    - bool: True if the path exists, False otherwise.
+    """
+    return os.path.exists(path)
+
+
+# ============================================================================
+
+
+def validate_input(msg, data_type):
+    while True:
+        user_input = input(msg)
+        if user_input.lower() == "exit":
+            input(Fore.RED + "Canceled,press enter to continue...")
+            return user_input
+        if data_type == "alfabetic" and user_input.isalpha():
+            return user_input
+        elif data_type == "numeric" and user_input.isdigit():
+            return user_input
+        elif data_type == "alfanumeric" and user_input.isalnum():
+            return user_input
+        elif data_type == "float":
+            try:
+                float(user_input)
+                return user_input
+            except ValueError:
+                pass
+        print(Fore.RED + f"\rInput should be {data_type}. Try again.", end="")
+        input(Fore.RED + " Press Enter to continue...")
+        print("\r" + " " * 100, end="\r")
+
+
+# ============================================================================
+
+
+def user_input(data_dict):
+    """
+    Collects user input for each key in a dictionary, excluding the 'ID' field.
+
+    Parameters:
+    -data_dict (dict): Dictionary template with keys representing data fields.
+
+    Returns:
+    -dict: A new dictionary populated with user input for each key.
+    """
+    new_data = {}
+    count = 0
+    print(Fore.YELLOW + "Write exit to cancel this action. ")
+    for key, value in data_dict.items():
+
+        if key == "ID":
+            new_data[key] = ""
+        else:
+            count += 1
+            msg = f"{Fore.CYAN}{count}: Enter {key:<13}:\t".replace(" ", "-")
+            data = validate_input(msg, value)
+            if data == "exit":
+                return data
+            new_data[key] = data
+    print()
+    return new_data
+
+
+# ============================================================================
+
+
+def add_car(new_data):
+    """
+    Adds a new car's details to the showroom data and writes it to a file.
+
+    Parameters:
+    - new_data (dict): A dictionary containing the car's details to add.
+    - after (optional): Parameter not used in the function but could be
+      for specifying position.
+    """
+    if len(new_data) != len(header):
+        return
+
+    global car_count
+    car_count = str(len(showroom_data) + 1)
+    new_data["ID"] = car_count
+    mode = "a" if os.path.exists(file_name) else "w"
+
+    with open(file_name, mode, encoding="utf-8") as file:
+
+        if mode == "w" or os.path.getsize(file_name) == 0:
+            file.write(f"{header_line}\n")
+
+        for key, value in new_data.items():
+            format_key = f"{key:<12}".replace(" ", "-")
+            file.write(f"{format_key}:\t{car_count if key=='ID' else value}\n")
+
+        file.write(f"{separator_line}\n")
+    showroom_data.append(new_data)
+    message = (
+        f"Look at that! Our showroom just got richer "
+        f"with its {num2words(car_count, to='ordinal_num')} car. "
+        "Press enter to continue this journey..."
+    )
+
+    input(Fore.LIGHTGREEN_EX + f"{message}")
+
+
+# ============================================================================
+
+
+def load_data(what_file=file_name, what_array=showroom_data):
+    """
+    Loads car data from a file into a list.
+
+    Parameters:
+    - what_file (str): The name of the file from which to load the data.
+    - what_array (list): The list to populate with the loaded car data.
+    """
+    loaded_data = {}
+    count = 0
+
+    readable = False
+
+    if os.path.exists(file_name):
+        readable = bool(os.path.getsize(file_name) > 0)
+    else:
+        with open(file_name, "w"):
+            pass
+
+    if readable:
+        with open(what_file, "r", encoding="utf-8") as file:
+            lines = file.readlines()
+            for line in lines:
+                if not line.startswith("="):
+                    key, value = line.split(":")
+                    loaded_data[key.strip("-")] = value.strip("\n")
+                elif not any(char.isalpha() for char in line):
+                    count += 1
+                    what_array.append(loaded_data)
+                    loaded_data = {}
+
+
+# ============================================================================
+
+
+def save_data(file_path=file_name, cars=showroom_data):
+    """
+    Saves the car data to a specified file.
+
+    Parameters:
+    - file_path: The file to write the car data to.
+    - cars: The list of car dictionaries to save.
+    """
+    with open(file_path, "w", encoding="utf-8") as file:
+        local_car_count = 0
+        file.write(f"{header_line}\n")
+        for car in cars:
+            local_car_count += 1
+            car["ID"] = str(local_car_count)
+            for key, value in car.items():
+                format_key = f"{key:<12}".replace(" ", "-")
+                if key == "ID":
+                    car_value = str(local_car_count)
+                else:
+                    car_value = value.strip()
+                file.write(f"{format_key}:\t{car_value}\n")
+            file.write(f"{separator_line}\n")
+
+
+# ============================================================================
+
+
+def modify_car(target_car):
+    """
+    Replaces an existing car's details in the showroom data with new details.
+
+    Parameters:
+    - file_name (str): The name of the file where the data is saved.
+    - target_car (int): The ID of the car to modify.
+    - new_dictionary (dict): A dictionary containing the new car details.
+    """
+    while not target_car.isdigit():
+        target_car = input(
+            Fore.RED + "Input should be a number, enter car-ID again: /n"
+        )
+    while int(target_car) <= 0 or int(target_car) > int(car_count):
+        target_car = input(
+            Fore.RED + "Can't find this ID,enter"
+            + " number from 0 to {car_count}:\n"
+        )
+    data = user_input(header)
+    if len(data) != len(header):
+        return
+    new_dictionary = data
+    showroom_data.pop((int(target_car) - 1))
+    showroom_data.insert(int(target_car) - 1, new_dictionary)
+    save_data()
+    input(Fore.RED + f"Id {target_car} successfully modified,"
+          + "press enter to continue.")
+
+
+# ============================================================================
+
+def remove_car(target_car):
+    """
+    Removes a car from the showroom data based on its ID.
+
+    Parameters:
+    - target_car (int): The ID of the car to remove.
+    """
+    if target_car == "exit":
+        input(Fore.RED + "Canceled,press enter to continue...")
+        return
+    global car_count
+    while int(target_car) > int(car_count) or int(target_car) <= 0:
+        target_car = input(
+            Fore.RED + "Can't find this ID,enter number"
+            + f" from 0 to {car_count}:\n"
+        )
+
+    showroom_data.pop((int(target_car) - 1))
+    save_data()
+    input(
+        Fore.RED + f"Id {target_car} successfully deleted ,press enter"
+        " to continue..."
+    )
+
+
+# ============================================================================
+
+
+def show_car():
+    """
+    Displays all cars' details from the showroom data.
+    Clears the screen before displaying and waits for user input to continue.
+    """
+    clear_screen()
+    print(Fore.LIGHTGREEN_EX + header_line)
+    for car in showroom_data:
+        for key, value in car.items():
+            print(Fore.YELLOW + f"{key:<12}---{value}".replace(" ", "-"))
+        print(Fore.LIGHTGREEN_EX + separator_line)
+    print(Fore.GREEN)
+    input("Press enter to continue...")
+
+
+# ============================================================================
+
+
+def exit_showroom():
+    """
+    Displays a farewell message and exits the program.
+    """
+    clear_screen()
+    farewell_message = (
+        Fore.RED + "  Thank you for using the Showroom Manager!\n\n"
+        "  Wishing you all the best!"
+    )
+    print(farewell_message)
+    sleep(4)
+    clear_screen()
+
+
+# ============================================================================
+
+
+def welcome_message():
+    """
+    Prints a welcome message for the Showroom Manager application.
+    """
+    clear_screen()
+    welcome_text = (
+        f"{Fore.YELLOW}Welcome to the Showroom Manager!\n"
+        "This application helps you manage \na car"
+        "showroom's inventory. You can \nadd, modify, delete, and view cars\n"
+        "in the showroom. Navigate through \nthe menu by selecting"
+        "the corresponding \nnumber of the action you wish to \nperform."
+        "Let's make managing your \nshowroom inventory "
+        "effortless and efficient!"
+    )
+    print(welcome_text)
+
+
+# ============================================================================
+
+
+def main():
+    """
+    The main function to run the showroom manager program.
+    It presents a menu to the user and processes the selected action.
+    """
+    global car_count
+    welcome_message()
+    input("\nPress enter to start...")
+    clear_screen()
+    load_data(file_name, showroom_data)
+    car_count = len(showroom_data)
+    while True:
+        clear_screen()
+        print(Fore.LIGHTGREEN_EX + "Showroom Manager:\n")
+        print(menu_str)
+        choice = input(Fore.LIGHTGREEN_EX + "Select action from 1 to 6 : ")
+        print()
+        try:
+            if choice < "1" or choice > "6":
+                raise ValueError("Please select a valid option from 1 to 6.")
+
+            if choice == "1":
+                add_car(user_input(header))
+            elif choice == "2":
+                modify_car(input(Fore.CYAN + "Enter car id  to modify:\t"))
+            elif choice == "3":
+                remove_car(
+                    input(
+                        Fore.YELLOW + "Enter car id for delete"
+                        + "or exit to cancell :\t"
+                    )
+                )
+            elif choice == "4":
+                show_car()
+            elif choice == "5":
+                msg_count_car()
+            elif choice == "6":
+                exit_showroom()
+                break
+            else:
+                print("Invalid selection. Please try again.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            sleep(2)
+
+
+# ============================================================================
+
+if __name__ == "__main__":
+    main()
